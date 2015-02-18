b6b6e90711ff677c80aba62325a7ff3689a4e2f5
Jim Safley <jimsafley@gmail.com>
2015-02-18 17:44:21 -0500

    Do not separate request validation from action.

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -26,14 +26,27 @@ class IndexController extends AbstractActionController
                     'apiKey' => $data['apiKey'],
                 );
 
-                if ($this->isValidRequest($args)) {
+                // Validate a Zotero API request.
+                $uri = new Uri($args['type'], $args['id']);
+                $uri->setCollectionKey($args['collectionKey']);
+                $uri->setLimit(1);
+                $headers = array();
+                if ($args['apiKey']) {
+                    $headers['Authorization'] = sprintf('Bearer %s', $args['apiKey']);
+                }
+                $client = $this->getServiceLocator()->get('Omeka\HttpClient');
+                $client->setHeaders($headers);
+                $response = $client->setUri($uri->getUri())->send();
+
+                if ($response->isSuccess()) {
                     $dispatcher = $this->getServiceLocator()->get('Omeka\JobDispatcher');
                     $dispatcher->dispatch('ZoteroImport\Job\Import', $args);
-                    // Clear the form.
-                    $form = new ImportForm($this->getServiceLocator());
+                    $form = new ImportForm($this->getServiceLocator()); // Clear the form.
                     $this->messenger()->addSuccess('Importing from Zotero');
                 } else {
-                    $this->messenger()->addError('The requested Zotero library or collection is invalid.');
+                    $this->messenger()->addError(sprintf(
+                        'Error when requesting Zotero library: %s', $response->getReasonPhrase()
+                    ));
                 }
             } else {
                 $this->messenger()->addError('There was an error during validation');
@@ -43,28 +56,5 @@ class IndexController extends AbstractActionController
         $view = new ViewModel;
         $view->setVariable('form', $form);
         return $view;
-    }
-
-    /**
-     * Validate a Zotero API request.
-     *
-     * @param array $args
-     * @return bool
-     */
-    public function isValidRequest(array $args)
-    {
-        $uri = new Uri($args['type'], $args['id']);
-        $uri->setCollectionKey($args['collectionKey']);
-        $uri->setLimit(1);
-
-        $headers = array();
-        if ($args['apiKey']) {
-            $headers['Authorization'] = sprintf('Bearer %s', $args['apiKey']);
-        }
-        $client = $this->getServiceLocator()->get('Omeka\HttpClient');
-        $client->setHeaders($headers);
-
-        $response = $client->setUri($uri->getUri())->send();
-        return $response->isSuccess();
     }
 }
